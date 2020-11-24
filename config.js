9a01d150eb07dabcfd3e7005f3121844a814dde5
Kristy Li <kristyli20.0@gmail.com>
2020-11-23 20:52:29 -0500

    chore: Update configuration file

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/images/favicon.png',
-  pathPrefix: `/gatsby-starter-spectral/`,
+  pathPrefix: `/`,
   heading: 'Kristy Li',
   subHeading: 'Fullstack Developer',
   socialLinks: [
